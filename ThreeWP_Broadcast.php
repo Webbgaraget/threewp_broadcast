6bfd9df617e93fb1f4d24c79572b4a6e71736d91
Edward Plainview <edward@plainview.se>
2014-06-18 12:54:52 +0200

    More debug text.

--- ThreeWP_Broadcast.php
+++ ThreeWP_Broadcast.php
@@ -2452,9 +2452,15 @@ This can be increased by adding the following to your wp-config.php:
 					$o = clone( $bcd );
 					$o->attachment_data = $attachment;
 					if ( $o->attachment_data->post->post_parent == $bcd->post->ID )
+					{
+						$this->debug( 'Assigning new parent ID (%s) to attachment %s.', $bcd->new_post()->ID, $o->attachment_data->post->ID );
 						$o->attachment_data->post->post_parent = $bcd->new_post[ 'ID' ];
+					}
 					else
+					{
+						$this->debug( 'Reseting post parent for attachment %s.', $o->attachment_data->post->ID );
 						$o->attachment_data->post->post_parent = 0;
+					}
 					$this->maybe_copy_attachment( $o );
 					$a = new \stdClass();
 					$a->old = $attachment;
@@ -2598,9 +2604,15 @@ This can be increased by adding the following to your wp-config.php:
 					$o->attachment_data = $bcd->attachment_data[ 'thumbnail' ];
 
 					if ( $o->attachment_data->post->post_parent == $bcd->post->ID )
+					{
+						$this->debug( 'Assigning new parent ID (%s) to attachment %s.', $bcd->new_post()->ID, $o->attachment_data->post->ID );
 						$o->attachment_data->post->post_parent = $bcd->new_post[ 'ID' ];
+					}
 					else
+					{
+						$this->debug( 'Reseting post parent for attachment %s.', $o->attachment_data->post->ID );
 						$o->attachment_data->post->post_parent = 0;
+					}
 
 					$this->debug( 'Custom fields: Maybe copying attachment.' );
 					$this->maybe_copy_attachment( $o );
